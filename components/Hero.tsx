e079d95c76032e9bdf0f1572b4aaf5d7a425b332
Alex Kashkarian <alexkashkarian@Alexs-MacBook-Air.local>
2026-02-03 12:56:13 -0800

    Increase dashboard image size by 50% (105vh to 157.5vh)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -316,7 +316,7 @@ export default function Hero({ isAIChatOpen }: HeroProps) {
               src={`/dashboard-${activeTab + 1}.png`}
               alt={`Dashboard ${activeTab + 1}`}
               className="w-full h-auto object-contain"
-              style={{ maxHeight: '105vh', maxWidth: '90%' }}
+              style={{ maxHeight: '157.5vh', maxWidth: '90%' }}
             />
           </div>
 
